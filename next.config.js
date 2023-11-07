cce00c0d76d5830b6580aef61ef2d433e1065797
Yasin Silavi <ys.silavi@gmail.com>
2023-11-08 00:14:16 +0330

    chore: change base path config in production

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const { withContentlayer } = require('next-contentlayer')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/blog' : '',
   reactStrictMode: true,
 }
 
