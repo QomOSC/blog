32fda707e64aeab94e2d3fa49772c60ee7421613
Themmr <you@example.com>
2024-01-14 10:14:38 +0330

    add header to all  website an not the landing

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,17 @@
+import Header from "./components/Header"
 import Landing from "./landing/page"
-
 <link
   rel="icon"
   href="/icon?<generated>"
   type="image/<generated>"
   sizes="<generated>"
 />
-// function PostCard(post: Post) {
-//   return (
-//     <div className="mb-8">
-//       <h2 className="mb-1 text-xl">
-//         <Link href={post.url} className="text-blue-700 hover:text-blue-900 dark:text-blue-400">
-//           {post.title}
-//         </Link>
-//       </h2>
-//       <time dateTime={post.publishedAt} className="mb-2 block text-xs text-gray-600">
-//         {format(parseISO(post.publishedAt), 'LLLL d, yyyy')}
-//       </time>
-//       <div className="text-sm [&>*]:mb-3 [&>*:last-child]:mb-0" dangerouslySetInnerHTML={{ __html: post.body.raw }} />
-//     </div>
-//   )
-// }
 
 export default function Home() {
 
   return (
     <div className="main">
+      <Header></Header>
       <Landing></Landing>
     </div>
   )
