cfb70ebe4bb0a553158c66195e2b28182889dfef
Themmr <you@example.com>
2024-01-14 10:15:01 +0330

    disbled image optimization

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
-const { withContentlayer } = require('next-contentlayer')
+const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/blog' : '',
+  output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "/blog" : "",
   reactStrictMode: true,
-}
 
-module.exports = withContentlayer(nextConfig)
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = withContentlayer(nextConfig);
