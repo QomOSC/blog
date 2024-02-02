c4a771f61af1c24f4f65d7baaedcf6a4d4908855
Yasin Silavi <ys.silavi@gmail.com>
2024-02-02 16:17:36 +0330

    chore: add daisyui base configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,14 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
+  plugins: [require("daisyui")],
+
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+
   theme: {
     extend: {
       colors: {
@@ -18,6 +21,9 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+
+  daisyui: {
+    themes: ['light'],
+  },
 }
 export default config
